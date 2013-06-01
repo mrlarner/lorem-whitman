e3c858a3a7e08f041b3ef8017a19b323a0525513
Matt Larner <mrlarner@gmail.com>
2013-05-31 23:53:53 -0700

    fixed word splitting and added words to bin

--- lib/whitman/word.rb
+++ lib/whitman/word.rb
@@ -2,11 +2,11 @@ module Whitman
 	class Word < Base
 		
 		def self.word n = 1
-			self.split_source(" ").sample
+			self.split_source(nil, /[\s,]+/).sample
 		end
 
 		def self.words n = 3
-			self.split_source(" ").sample(n)
+			self.split_source(nil, /[\s,]+/).sample(n)
 		end
 	end
 end
