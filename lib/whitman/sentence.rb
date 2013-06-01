79a3c8766c33198791233a1fb746d15064861aa6
Matt Larner <mrlarner@gmail.com>
2013-05-31 21:03:15 -0700

    updated Sentence methods to use split_source

--- lib/whitman/sentence.rb
+++ lib/whitman/sentence.rb
@@ -2,11 +2,11 @@ module Whitman
 	
 	class Sentence < Base
 		def self.sentence
-			self.processed_source.sample
+			self.split_source.sample
 		end
 		
 		def self.sentences n = 5
-			self.processed_source.sample(n)
+			self.split_source.sample(n)
 		end
 	end
 end
