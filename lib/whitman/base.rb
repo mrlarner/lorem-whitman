5c6dfab014b6ba8060bd0c06ee9c2ff3817cf71f
Matt Larner <mrlarner@gmail.com>
2013-05-31 21:02:26 -0700

    allow choosing or defaulting file and split char

--- lib/whitman/base.rb
+++ lib/whitman/base.rb
@@ -1,18 +1,30 @@
 module Whitman
 	class Base
-		def self.source
-			@source ||= self.read
+
+		def self.file f = nil
+			if f.nil?
+				@file = 'song-of-myself.txt'
+			else
+				@file = f
+			end
+		end
+
+		def self.source f = nil
+			@source ||= self.read f
 		end
 		
-		def self.processed_source
-			@processed_source ||= self.source.split("\n").uniq
+		def self.split_source f = nil, split = "\n"
+			@split_source ||= self.source(f).split(split).keep_if { |line| ! line.start_with? "#" or ! line.start_with? "-----" }.uniq
 		end
 		
 		private
 		
-		def self.read
-			File.read File.join(File.dirname(File.expand_path(__FILE__)), "..", "..", "text", "song-of-myself.txt")
+		def self.read f = nil
+			f = 'song-of-myself.txt' if f.nil?
+			f = 'song-of-myself.txt' unless File.exists? f
+			File.read File.join(File.dirname(File.expand_path(__FILE__)), "..", "..", "text", f)
 		end
+		
 
 	end
 
--- spec/lib/whitman/base_spec.rb
+++ spec/lib/whitman/base_spec.rb
@@ -3,12 +3,35 @@ require_relative '../../spec_helper.rb'
 describe Whitman::Base do
 	subject { Whitman::Base }
 	describe "when reading from file" do
-		it "must have a source" do
+		it "should have a source" do
 			subject.should respond_to :source
 		end
 
-		it "must at least have song of myself as source" do
+		it "should have a string as source" do
 			subject.source.should be_an_instance_of String
+		end
+
+		it "should be able to set file" do
+			subject.file.should be
+			subject.file("another-source").should be
+			subject.file("another-source").should be == "another-source"
+			subject.file.should_not be subject.file("another-source")
+		end
+
+		it "should always fin" do
+			subject.file
+		end
+	end
+
+	describe "when processing a file" do
+		it "should let you choose how to split it" do
+			subject.split_source.should be
+			subject.split_source(" ").should be
+		end
+
+		it "should return an array of strings" do
+			subject.split_source.should be_an_instance_of Array
+			subject.split_source.first.should be_an_instance_of String
 		end
 	end
 end
