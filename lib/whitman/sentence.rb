1e854943f7bb3d9cfff3a0a225a43fe36096e288
Matt Larner <mrlarner@gmail.com>
2013-05-31 21:40:41 -0700

    made sentences smarter
    can choose one, any, or all sentences

--- lib/whitman/sentence.rb
+++ lib/whitman/sentence.rb
@@ -1,12 +1,19 @@
 module Whitman
-	
 	class Sentence < Base
-		def self.sentence
-			self.split_source.sample
+		def self.sentence x = nil
+			if x.nil? or not x.kind_of? Integer
+				self.split_source.sample
+			else
+				self.split_source[x]
+			end
 		end
 		
 		def self.sentences n = 5
-			self.split_source.sample(n)
+			if n.to_i < 0
+				self.split_source
+			else
+				self.split_source.sample(n || 5)
+			end
 		end
 	end
 end
--- spec/lib/whitman/sentence_spec.rb
+++ spec/lib/whitman/sentence_spec.rb
@@ -4,11 +4,20 @@ describe Whitman::Sentence do
 
 	subject { Whitman::Sentence }
 
-	it "must return a sentence" do
+	it "should return a sentence" do
 		subject.sentence.should be_an_instance_of String
+	end
+	
+	it "should allow you to pick sentence within range or sentences" do
+		subject.sentence(1).should be_an_instance_of String
+		subject.sentence(subject.sentences(-1).length + 1).should_not be
 	end
 
 	it "should return 5 sentences by default" do
 		subject.sentences.size.should be == 5
+	end
+
+	it "should return requested number of sentences" do
+		subject.sentences(12).length.should be == 12
 	end
 end
